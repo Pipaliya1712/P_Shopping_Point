25857219e243935b791238aa164d41824da038c3
Parth Pipaliya <parthpipaliya1712@gmail.com>
2023-10-31 22:22:37 +0530

    some modifications

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,27 +1,6 @@
-let buyblazer = document.getElementsByClassName("buyblazer")
-for (let i = 0 ; i< buyblazer.length;i++){
-    buyblazer[i].onclick = () => {
-        window.open('buyblazer.html' , '_self');
-    }
+let buyblazer = document.querySelectorAll('.buyblazer')
+for(let i=0;i<buyblazer.length;i++){
+    buyblazer[i].addEventListener('click' , e = () => {
+        window.open('../buyblazer.html' , '_self');
+    });
 }
-
-const first = document.querySelector('.first');
-console.log(first)
-
-select = (ulEl , aEl) => {
-    Array.from(ulEl , aEl).forEach(
-        (v) => {
-            v.firstChild.classList.remove('fb');
-        }
-    );
-    if(aEl) aEl.classList.add('fb');
-}
-
-first.addEventListener('click', e => {
-    const selected = e.target;
-    select(first, selected);
-});
-
-
-
-
