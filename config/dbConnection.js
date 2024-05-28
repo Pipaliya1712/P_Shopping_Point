09178ea586e23dab999256c293d61eac9274047a
Parth Pipaliya <parthpipaliya1712@gmail.com>
2024-05-28 16:23:03 +0530

    Reset database connectivity: Updated MongoDB URI in the environment configuration

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async() => {
     try{
-        const connect = await mongoose.connect("mongodb://localhost:27017/p_shopping_point",{useNewUrlParser: true, useUnifiedTopology:true})
+        const connect = await mongoose.connect(process.env.MONGO_DB,{useNewUrlParser: true, useUnifiedTopology:true})
         console.log(
             "Database connected: ",
             connect.connection.host,
