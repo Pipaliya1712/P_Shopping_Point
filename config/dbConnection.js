d6ce6d35ca77af78c3e2aa0c6e8d746a2bc461f9
Parth Pipaliya <parthpipaliya1712@gmail.com>
2024-05-28 16:37:49 +0530

    Adjust deployment schedule

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const connectDB = async() => {
     try{
-        const connect = await mongoose.connect(process.env.MONGO_DB,{useNewUrlParser: true, useUnifiedTopology:true})
+        const connect = await mongoose.connect("mongodb+srv://parthpipaliya1112:bUINZfw1yl7uVnzM@pshoppingpoint.xsg1r7k.mongodb.net/P_Shopping_Point?retryWrites=true&w=majority&appName=PShoppingPoint",{useNewUrlParser: true, useUnifiedTopology:true})
         console.log(
             "Database connected: ",
             connect.connection.host,
